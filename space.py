8f3e664eb9660524c2408dae1fce9d4d22ea228a
Priya7075 <102401793+Priya7075@users.noreply.github.com>
2022-03-31 21:56:21 +0530

    Add files via upload

---
+++ space.py
@@ -0,0 +1,46 @@
+# a=''
+# print(a.isspace()) #false
+# b=" "
+# print(b.isspace()) #true
+# c='.     '
+# print(c.isspace()) #false
+# d='           '
+# print(d.isspace()) #true
+# e="kaveri"
+# print(e.isspace()) #false
+# f=""""kaveri"""
+# print(f.isspace()) #false
+
+# a=1
+# print(type(a)) # int
+# b="c"
+# print(type(b)) # str
+# a=2.2
+# print(type(a)) # float
+# c=None
+# print(type(c)) # NoneType
+# d=True
+# print(type(d)) # bool
+# e=False
+# print(type(e))
+
+
+# print("hello"*4) #hellohellohellohello
+# print("hello "*2) #hello hello
+# print(" hello"*2) # hello hello
+# print('hello'*'hello') # typerror: can't multiply seqence by non-int of type str
+# print('hello'*2.4)  # typerror: can't multiply seqence by non-int of type float
+
+#print('python'+2) # can only concatenate str (not "int") to str
+# print('pyt'+'hon') #python
+# print('py '+'thon') # py thon
+# print('py'+' thon') #py thon
+# print('py'+''+'thon') #python
+# print('py'+'    '+'thon') #py    thon
+# print("""py'"""+"""'thon""") #py''thon
+# print("'py"+"'thon") #'py'thon
+print('py'+'*'+'thon') # py*thon
+print('py'+'-'+'thon') #py-thon
+print('T'+' '+"koteswari") #T koteswari
+print('T'+'.'+"koteswari") #T.koteswari
+
